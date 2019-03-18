c4a9e300e345d4000ead7c3099a9bcb2a71a1550
JihaneBennis <bennis.jihane@hotmail.fr>
2019-03-18 17:59:57 +0100

    fix typo

--- models/gender_classifier.py
+++ models/gender_classifier.py
@@ -11,7 +11,7 @@ K = tf.keras
 def gender_clf_model(encoding_dim):
     representation = K.Input(shape=(encoding_dim,))
     hidden_layer=K.layers.Dense(512, activation='relu')(representation)
-    output_gender=K.layers.Dense(shape=(2), activation='sigmoid')(hidden_layer)
+    output_gender=K.layers.Dense(units=2)(hidden_layer)
 
     gender_clf = K.Model(representation, output_gender)
     
