7e82a68270b4c9ae75b05f2a1d11a6ae2c0ea92b
JihaneBennis <bennis.jihane@hotmail.fr>
2019-03-18 18:25:03 +0100

    code cleaning

--- models/gender_classifier.py
+++ models/gender_classifier.py
@@ -1,18 +1,14 @@
-import os
-import numpy as np
 import tensorflow as tf
-import pandas as pd
-
-from operator import itemgetter
 
 K = tf.keras
 
 
 def gender_clf_model(encoding_dim):
     representation = K.Input(shape=(encoding_dim,))
-    hidden_layer=K.layers.Dense(512, activation='relu')(representation)
-    output_gender=K.layers.Dense(units=2)(hidden_layer)
+
+    hidden_layer = K.layers.Dense(512, activation='relu')(representation)
+    output_gender = K.layers.Dense(units=2)(hidden_layer)
 
     gender_clf = K.Model(representation, output_gender)
-    
+
     return gender_clf
